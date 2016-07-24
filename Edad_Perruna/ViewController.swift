bad7facd0894e3bdd0caae09eec8d1a49e7d2f69
Juan Morillo <juanmorillios@gmail.com>
2016-07-24 23:00:46 +0200

    App Edad Perruna
    App Edad Perruna

--- Edad_Perruna/ViewController.swift
+++ Edad_Perruna/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let humanYear = 7
 
+  
+    @IBOutlet weak var entrarEdadPerroTextField: UITextField!
+    @IBOutlet weak var edadDelPerroEsLabel: UILabel!
+    @IBOutlet weak var mainContentImageView: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        edadDelPerroEsLabel.text = "Edad de su perro:"
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func calcAgeDog(_ sender: UIButton) {
+        
+        
+        processedData()
+        
+        
     }
+    
+    func processedData(){
+    
+    
+    let valueTextField = Int(entrarEdadPerroTextField.text!)
+    
+    edadDelPerroEsLabel.text = "La edad de su perro es:\(valueTextField! * humanYear) años"
+        
+        if mainContentImageView.isHidden {
+        
+        
+           mainContentImageView.isHidden = false
+        }
+    
+    }
+    
+    @IBAction func infoButtonPressed(_ sender: UIButton) {
+        
+        alert()
+        
+    }
+    
+    func alert(){
+    
+        let alertViewController = UIAlertController(title: "Calculadora Edad Perruna🐶", message: "App que realiza el cálculo de la edad de un 🐶 comparándola con la edad de una 👱🏿, donde 1 año en la edad de un perro equivale a 7 años de un humano, según dicen.", preferredStyle: .alert)
+        
+        let OkAction = UIAlertAction(title: "Cerrar", style: .destructive, handler: nil)
+        
+        alertViewController.addAction(OkAction)
+        
+        present(alertViewController, animated: true, completion: nil)
 
+        print("\(alertViewController.message)")
+    
+    }
+    
 
 }
 
